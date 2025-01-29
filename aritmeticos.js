b62167f09660f3bad90ccbca1ddc4c92b5a05d45
soouza72 <biarubiasouza@gmail.com>
2025-01-29 08:18:58 -0300

    Initial commit

---
+++ aritmeticos.js
@@ -0,0 +1,24 @@
+// console.log("Olá, Mundo!")
+
+// para declarar variáveis
+let nome = "Juliana" //vamos preferir esse
+var nomes = "Juliana"
+
+// CTRL + ; -> COMENTARIOS
+console.log(nome)
+
+let idade = 24
+console.log("Minha idade é ", idade, " anos")
+// CONCATENAÇÃO = juntar texto e valor
+
+
+// OPERAÇÕES MATEMÁTICAS
+
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b 
+let resto = a % b
